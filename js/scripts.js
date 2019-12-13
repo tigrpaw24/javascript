a0afd7e0a661505fc4947dadab54261074e94d13
Jennifer Lewis <tigrpaw24@yahoo.com>
2019-12-12 20:54:24 -0800

    add javascript numbers strings, array, Object

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+//Numbers
+//numbers and be computed with =, -, *, /, %, ==increment, --decrement
+//NaN means Not a Number
+var pizza = 5;
+var soda=1;
+var bread=3;
+
+var total = pizza + soda
+
+console.log(total);
+/** 
+ * The console. log() is a function in JavaScript which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user.
+*/
+
+//String
+
+const name = 'Jen Lewis';
+
+const welcome = "Welcome to Geekwise Academy";
+
+
+
+
+console. log(name);
+console. log(welcome);
+
+//Array
+var fruits = ['apples', 'grapes', 'oranges', 'bananas']
+var rockBand = ['Pantera', 'Tool', 'Metallica', 'Rush'];
+
+
+console. log(fruits);
+console. log(rockBand);
+
+//Object
+
+var car = {
+    type:"flat",
+    model:"500",
+    color:"white"
+};
+
+console. log(car);
